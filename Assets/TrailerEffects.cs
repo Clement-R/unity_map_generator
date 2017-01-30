4ccdac59df7dc35bc0a77bc7567754345292c602
C.Rondeau <rondeau.clement@gmail.com>
2017-01-30 23:48:04 +0100

    Final version

---
+++ Assets/TrailerEffects.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailerEffects : MonoBehaviour {
+    public Sprite[] soldiers;
+
+    int index;
+
+	void Update () {
+        SpriteRenderer player = GameObject.Find("Soldier_0_1").GetComponent<SpriteRenderer>();
+
+		if(Input.GetKeyDown(KeyCode.Space)) {
+            player.sprite = soldiers[index];
+            index = Mathf.Clamp(index + 1, 0, soldiers.Length - 1);
+        }
+	}
+}
